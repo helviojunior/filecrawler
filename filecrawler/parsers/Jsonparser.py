349413f06dfd5896c46935f987848bb90e5412a3
Helvio Junior <helvio_junior@hotmail.com>
2023-03-17 17:38:38 -0300

    Improvments

--- filecrawler/parsers/Jsonparser.py
+++ filecrawler/parsers/Jsonparser.py
@@ -1,20 +1,33 @@
 import json
+import os
+import sys
 from pathlib import Path
 
 from filecrawler.libs.file import File
 from filecrawler.libs.parser import Parser
+from filecrawler.libs.process import Process
 from filecrawler.util.color import Color
 from filecrawler.util.logger import Logger
 from filecrawler.parserbase import ParserBase
 
 
-class DefaultParser(ParserBase):
+class JsonParser(ParserBase):
+    extensions = ['json']
+    mime_types = ['application/json']
 
     def __init__(self):
-        super().__init__('Default', 'Parser for PDF files')
+        super().__init__('JSON Parser', 'Parser for JSON files')
 
     def parse(self, file: File) -> dict:
         from filecrawler.config import Configuration
         data = {'content': self.get_readable_data(file)}
 
+        if Configuration.json_support:
+            try:
+                data['content'] = json.dumps(json.loads(data['content']), sort_keys=False, identy=2)
+            except:
+                pass
+
         return data
+
+
---
+++ filecrawler/parsers/xmlparser.py
@@ -0,0 +1,37 @@
+import json
+import os
+import sys
+from pathlib import Path
+
+import xmltodict
+
+from filecrawler.libs.file import File
+from filecrawler.libs.parser import Parser
+from filecrawler.libs.process import Process
+from filecrawler.util.color import Color
+from filecrawler.util.logger import Logger
+from filecrawler.parserbase import ParserBase
+
+
+class XMLParser(ParserBase):
+    extensions = ['xml']
+    mime_types = ['text/xml']
+
+    def __init__(self):
+        super().__init__('XML Parser', 'Parser for XML files')
+
+    def parse(self, file: File) -> dict:
+        from filecrawler.config import Configuration
+        data = {'content': self.get_readable_data(file)}
+
+        if Configuration.xml_support:
+            try:
+                with open(file.path, 'r') as xml_file:
+                    data_dict = xmltodict.parse(xml_file.read())
+                    data['object_content'] = json.loads(data_dict)
+            except:
+                pass
+
+        return data
+
+
