4f7d5ece5611b8022a6323ba18de1d1567d4dcb7
Helvio Junior <helvio_junior@hotmail.com>
2023-03-24 19:25:53 -0300

    SQLite Parser

---
+++ filecrawler/parsers/sqliteparser.py
@@ -0,0 +1,31 @@
+import json
+
+from filecrawler.libs.database import Database
+from filecrawler.libs.file import File
+from filecrawler.parserbase import ParserBase
+
+
+class SQLite3Parser(ParserBase):
+    extensions = []
+    mime_types = ['application/vnd.sqlite3']
+
+    def __init__(self):
+        super().__init__('SQLite3 Parser', 'Parser for SQLite3 files')
+
+    def parse(self, file: File) -> dict:
+        from filecrawler.config import Configuration
+        from filecrawler.util.tools import Tools
+        content = {}
+
+        try:
+            with Database(db_name=file.path, auto_create=False) as db:
+                tables = db.select_raw('SELECT m.tbl_name AS table_name FROM sqlite_master AS m')
+                for t in tables:
+                    rows = db.select(t['table_name'], **{})
+                    content.update({t['table_name']: rows})
+        except:
+            pass
+
+        data = {'content': json.dumps(content, default=Tools.json_serial, sort_keys=False, identy=2)}
+        return data
+
