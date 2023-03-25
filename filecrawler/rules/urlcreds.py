fd1e878b339ca57e149375c5b7af7bcb50ff8c36
Helvio Junior <helvio_junior@hotmail.com>
2023-03-25 01:05:16 -0300

    URL Creds

---
+++ filecrawler/rules/urlcreds.py
@@ -0,0 +1,17 @@
+import re
+
+from filecrawler.rulebase import RuleBase
+
+
+class UrlCreds(RuleBase):
+
+    def __init__(self):
+        super().__init__('url-creds', 'URL Credentials')
+
+        self._regex = re.compile(r"([a-zA-Z0-9_-]{2,30}://[^@:]{1,256}:[^@:]{1,256}@[-a-zA-Z0-9:%._\\+~#?&//=]{2,256}\.[a-z]{2,6}[\.a-z]{0,6}[:0-9]{0,5})")
+        self._keywords = ["://"]
+
+        self._tps = [
+            self.generate_sample_secret("url", "http://user:pass@domain.com.br:8080"),
+            self.generate_sample_secret("url", "https://user:pass@domain.com:8080")
+        ]
