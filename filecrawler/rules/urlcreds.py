6f32ea38cdec68fe0182490c9fafa10689353e76
Helvio Junior <helvio_junior@hotmail.com>
2023-03-25 01:12:18 -0300

    URL Creds

--- filecrawler/rules/urlcreds.py
+++ filecrawler/rules/urlcreds.py
@@ -8,7 +8,7 @@ class UrlCreds(RuleBase):
     def __init__(self):
         super().__init__('url-creds', 'URL Credentials')
 
-        self._regex = re.compile(r"([a-zA-Z0-9_-]{2,30}://[^@:]{1,256}:[^@:]{1,256}@[-a-zA-Z0-9:%._\\+~#?&//=]{2,256}\.[a-z]{2,6}[\.a-z]{0,6}[:0-9]{0,5})")
+        self._regex = re.compile(r"([a-zA-Z0-9_-]{2,30}://[^@:]{1,256}:[^@:/]{1,256}@[-a-zA-Z0-9:%._\\+~#?&//=]{2,256}\.[a-z]{2,6}[\.a-z]{0,6}[:0-9]{0,5})")
         self._keywords = ["://"]
 
         self._tps = [
