61b840516fa54bf5ce6c81da718fac8ee6590c24
Helvio Junior <helvio_junior@hotmail.com>
2023-03-28 18:16:47 -0300

    Adjust URL creds rule

--- filecrawler/rules/urlcreds.py
+++ filecrawler/rules/urlcreds.py
@@ -11,6 +11,9 @@ class UrlCreds(RuleBase):
         self._regex = re.compile(r"([a-zA-Z0-9_-]{2,30}://[^@:]{1,256}:[^@:/\n\"']{1,256}@[a-zA-Z0-9._-]{2,256}[:0-9]{0,6})")
         # (?![A-Za-z0-9:._-])
         self._keywords = ["://"]
+        self._exclude_keywords = [
+            "\n"  # Cannot exists break line
+        ]
 
         self._tps = [
             self.generate_sample_secret("url", "http://user:pass@domain.com.br:8080"),
@@ -21,7 +24,8 @@ class UrlCreds(RuleBase):
 
         self._fps = [
             self.generate_sample_secret("url", "http://domain.com.br:8080/\nteste@123.com"),
-            self.generate_sample_secret("url", "http://domain.com.br:8080\nteste@123.com")
+            self.generate_sample_secret("url", "http://domain.com.br:8080\nteste@123.com"),
+            self.generate_sample_secret("url", "http://domain.com.br\nteste@123.com")
         ]
 
     def post_processor(self, original_data: str, found: str) -> dict:
