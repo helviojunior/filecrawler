85494c8f899837659360348233cf16922fc0e70b
Helvio Junior <helvio_junior@hotmail.com>
2025-01-09 01:14:41 -0300

    v0.1.17

--- filecrawler/__meta__.py
+++ filecrawler/__meta__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.16'
+__version__ = '0.1.17'
 __title__ = "FileCrawler"
 __description__ = "File Crawler index files and search hard-coded credentials."
 __url__ = "https://github.com/helviojunior/filecrawler"
